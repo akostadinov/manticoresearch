a441ba5312fb87a896fdc23de148a4083632f115
Aleksandar N. Kostadinov <akostadi@redhat.com>
2024-12-16 20:58:13 +0200

    some tmp changes to build on s390x

---
+++ dist/build_dockers/cross/sysroots/build_fedora40.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# this is root script which should be run to do everything
+# build for centos/redhat 8
+
+image=fedora:40
+distr=rhel9
+. redhat.sh
--- dist/build_dockers/cross/sysroots/debian.sh
+++ dist/build_dockers/cross/sysroots/debian.sh
@@ -1,8 +1,9 @@
 # file included from build_buster/bullseye/bookworm/focal/bionic/jammy
 # $distr and $image should be set on inclusion of this file
 
-arch=$(uname -m)
+# arch=$(uname -m)
+arch=s390x
 mkdir $arch
-docker run --rm -v $(pwd):/sysroot $image bash /sysroot/in_debian.sh $distr
+docker run --rm -v $(pwd):/sysroot --arch $arch --security-opt label=disable $image bash /sysroot/in_debian.sh $distr
 
 . finalize.sh
